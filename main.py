f97c13936a0f5537d375ae1168a6bc2f8c92f81c
HOMEWORK <alkhovik86@gmail.com>
2022-07-01 16:07:37 +0300

    Задача 3.2. Пользователь вводит три числа, найти среднее арифметическое с точностью 3.

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#Пользователь вводит три числа, найти среднее арифметическое с точностью 3.
+print('Введите первое число:')
+var1 = int(input())
+print('Введите второе число:')
+var2 = int(input())
+print('Введите третье число:')
+var3 = int(input())
+srednee = ((var1 + var2 + var3) / 3)
+print(srednee)
